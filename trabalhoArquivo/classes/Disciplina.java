6b1f5ce19535b82c1b82841765c6c3dde6b746aa
gabrielle-sudo <gabriellemartins88021@gmail.com>
2026-05-19 14:06:32 -0400

    Add files via upload

---
+++ trabalhoArquivo/classes/Disciplina.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Disciplina {
+    private String nome;
+    
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+}
